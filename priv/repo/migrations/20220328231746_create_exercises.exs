bd990a57fc70f64fbedc944427778be533189e15
Daniel Hessel <daniel@sharbe.com.br>
2022-03-28 20:41:36 -0300

    feat: creating training tables and exercises in the database

---
+++ priv/repo/migrations/20220328231746_create_exercises.exs
@@ -0,0 +1,15 @@
+defmodule Wabanex.Repo.Migrations.CreateExercises do
+  use Ecto.Migration
+
+  def change do
+    create table(:exercises) do
+      add :training_id, references(:trainings)
+      add :name, :string
+      add :youtube_video_url, :string
+      add :protocol_description, :string
+      add :repetitions, :string
+
+      timestamps()
+    end
+  end
+end
